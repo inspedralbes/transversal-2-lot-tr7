3724c65be9ab133c14bbe15063ef3eda567a01e1
Albert-de-la-Orza <A20albmar@inspedralbes.cat>
2022-11-29 14:04:43 +0100

    Carusel creat

--- web/front/js/script.js
+++ web/front/js/script.js
@@ -1,3 +1,44 @@
+Vue.component('carrousel', {
+    data: function() {
+        return {
+            slideIndex: 1,
+            numQuestions: 10
+        }
+    },
+    template: `<div>
+    <h2 class="w3-center">Manual Slideshow</h2>
+
+    <div class="w3-content w3-display-container">
+      <questions></questions>
+    
+      <button class="w3-button w3-black w3-display-left" @click="plusDivs(-1)">&#10094;</button>
+      <button class="w3-button w3-black w3-display-right" @click="plusDivs(1)">&#10095;</button>
+    </div>
+    </div>`,
+    
+    mounted(){
+        this.showDivs(this.slideIndex);
+    },
+
+    methods: {
+        plusDivs: function(n) {
+            this.showDivs(this.slideIndex += n);
+        },
+
+        showDivs: function(n) {
+            var questions = document.getElementsByClassName("mySlides");
+            if (n > questions.length) {this.slideIndex = 1}
+            if (n < 1) {this.slideIndex = questions.length}
+            for (i = 0; i < questions.length; i++) {
+                questions[i].style.display = "none";  
+            }
+            questions[this.slideIndex-1].style.display = "block";  
+        },
+
+        
+    } 
+});
+
 Vue.component('startdemo', {
   data: function () {
     return {};
@@ -38,7 +79,7 @@ Vue.component('questions', {
       <b-button @click="getQuestions" block variant="primary">Start Demo</b-button>
       <b-row>
         <b-col md="3" v-for="question in result">
-          <b-card>
+          <b-card class="mySlides">
             <quiz :game=question></quiz>
           </b-card>
         </b-col>
