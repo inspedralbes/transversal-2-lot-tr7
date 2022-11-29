fb52a74ce1a0dda5427df96c26c9a26f78728bff
ArnauDrets <a21arndrelli@inspedralbes.cat>
2022-11-29 13:51:00 +0100

    Vue

--- web/front/js/script.js
+++ web/front/js/script.js
@@ -1,17 +1,77 @@
-Vue.component('startdemo', {
-    data: function() {
-        return {
-        }
-    },
-template: `<div>
-        aaaaaa
+const index = Vue.component('startdemo', {
+  data: function () {
+    return {};
+  },
+  template: `<div>
+  <h1>Index</h1>
 </div>`,
+});
+Vue.component('quiz', {
+  props: ['game'],
+  template: `
+  <div>
+  {{game.question}}
+  {{randomNumber()}}
 
-})
+  <li v-for="incAns in game.incorrectAnswers">
+      {{ incAns }}
+  </li>
+  <li>
+    {{game.correctAnswer}}
+  </li>
+  </div>`,
+  methods: {
+    randomNumber: function () {
+      return Math.floor(Math.random() * 4);
+    },
+  },
+});
+const questionsDemo = Vue.component('questions', {
+  data: function () {
+    return {
+      result: [],
+    };
+  },
+  template: `
+    <div>
+      <h1>League of Trivial</h1>
+      <b-button @click="getQuestions" block variant="primary">Start Demo</b-button>
+      <b-row>
+        <b-col md="3" v-for="question in result">
+          <b-card>
+            <quiz :game=question></quiz>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>`,
+  methods: {
+    getQuestions: function () {
+      fetch(
+        'https://the-trivia-api.com/api/questions?categories=film_and_tv&limit=10&difficulty=easy'
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log({ data });
+          this.result = data;
+        });
+    },
+  },
+});
+
+const routes = [
+  {
+    path: '/',
+    component: index,
+  },
+  { path: '/jugarDemo', component: questionsDemo },
+];
+
+const router = new VueRouter({
+  routes,
+});
 
 let app = new Vue({
-    el: '#app',
-    data: {
-
-    },
+  el: '#app',
+  router,
+  data: {},
 });
